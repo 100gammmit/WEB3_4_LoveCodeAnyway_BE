f2db09c04912ecdc37412e2d2f7ba86ab525ae51
100gammmit <100minha@naver.com>
2026-04-20 22:18:03 +0900

    refactor: - s3 파일 삭제 비동기화

--- src/main/java/com/ddobang/backend/domain/upload/event/UploadEventListener.java
+++ src/main/java/com/ddobang/backend/domain/upload/event/UploadEventListener.java
@@ -1,5 +1,6 @@
 package com.ddobang.backend.domain.upload.event;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -20,20 +21,24 @@ import lombok.extern.slf4j.Slf4j;
 public class UploadEventListener {
 
 	private final S3UploadService s3UploadService;
+	private static final String EVENT_EXECUTOR = "s3EventExecutor";
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Async(EVENT_EXECUTOR)
 	public void onProfileImageChanged(ProfileImageChangedEvent event) {
 		log.info("프로필 이미지 삭제 : {}", event.oldUrl());
 		s3UploadService.delete(event.oldUrl());
 	}
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Async(EVENT_EXECUTOR)
 	public void onDiaryImageChanged(DiaryImageChangedEvent event) {
 		log.info("방탈출 일지 이미지 삭제 : {}", event.oldUrl());
 		s3UploadService.delete(event.oldUrl());
 	}
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Async(EVENT_EXECUTOR)
 	public void onAttachmentChanged(PostAttachmentsUpdatedEvent event) {
 		event.oldUrls().forEach(s3UploadService::delete);
 	}
